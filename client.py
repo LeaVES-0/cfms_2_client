6b84f2fbf7bdafabbc657a314060d9552d41091d
LeaVES <3059342645@qq.com>
2023-07-05 21:40:21 +0800

    fix some bugs

--- client.py
+++ client.py
@@ -20,23 +20,23 @@ class Client():
     def __init__(self, host, port=DEFAULT_PORT):
         pathlib.Path(f'./{DEFAULT_PEM_DIR}').mkdir(parents=True, exist_ok=True)
         self.host, self.port = host, port
-        self.clientstate = {"errorcode":0}
+        self.pemfile = pathlib.Path(f"{DEFAULT_PEM_DIR}/{self.host}_{self.port!s}.pem")
+        self.clientstate = False
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
         socket.setdefaulttimeout(7)
 
-    def r_pemfile(self, dir:str=DEFAULT_PEM_DIR):
+    def r_pemfile(self):
         """读公钥文件"""
-        pemfile = pathlib.Path(f"{dir}/{self.host}:{self.port!s}.pem")
-        pemfile.touch(exist_ok=True)
-        pemfileContent = pemfile.read_text(encoding="utf-8")
+        print(f"{dir}/{self.host}:{self.port!s}.pem")
+        self.pemfile.touch(exist_ok=True)
+        pemfileContent = self.pemfile.read_text(encoding="utf-8")
         return pemfileContent
 
-    def w_pemfile(self, pem:str, dir:str=DEFAULT_PEM_DIR):
+    def w_pemfile(self, pem:str):
         """写公钥文件"""
-        pemfile = pathlib.Path(f"{dir}/{self.host}:{self.port!s}.pem")
-        pemfile.write_text(data=pem, encoding="utf-8")
+        self.pemfile.write_text(data=pem, encoding="utf-8")
 
     def AES_encrypt(self, data):
         """AES加密"""
@@ -54,29 +54,24 @@ class Client():
 
     def recvall(self):
         self.client.settimeout(7)
-        while True:
-            try:
+        try:
+            while True:
                 primaryData, more = b"", b""
                 more = self.client.recv(1024)
                 primaryData += more
                 if len(more) < 1024:
                     return json.loads(self.AES_decrypt(primaryData))
-            except TimeoutError as e:
-                self.clientstate =  {'errorcode': 102, 'error': e}
+        except TimeoutError as e:
+            print("Time out.")
+            return str(e)
 
     def connectServer(self):
         """连接到服务器"""
-        try:
-            self.client.connect((self.host, self.port))
-        except ConnectionRefusedError as e:
-            self.clientstate =  {'errorcode': 100, 'error': e}
-        except ConnectionError as e:
-            self.clientstate =  {'errorcode': 100, 'error': e}
+        self.client.connect((self.host, self.port))
         self.client.sendall("hello".encode())
         self.client.recv(1024)
         self.client.sendall("enableEncryption".encode())
         public_key = json.loads(self.client.recv(1024).decode(encoding="utf-8"))['public_key']
-        self.r_pemfile()
         if public_key == (localkey:=self.r_pemfile()): 
             print(f"The public key is \n {public_key}")
         else:
@@ -94,7 +89,7 @@ class Client():
         serverResponse = json.loads(self.AES_decrypt(self.client.recv(1024)))
         if serverResponse['code'] == 0:
             print(f"Server state: {serverResponse['code']}. \nLink successful.")
-            self.clientstate = {'errorcode': 300}
+            self.clientstate = True
         
     def userLogin(self, username, password):
         """用户登陆"""
@@ -112,14 +107,10 @@ class Client():
             }
         
         self.client.send(self.AES_encrypt(json.dumps(request_data)))
-
-    def clientClose(self):
-        self.client.close()
-    
+        self.state_token = self.recv_data
     recv_data = property(recvall,)
 
 if __name__ == "__main__":
     cli = Client('127.0.0.1')
     cli.connectServer()
-    cli.userLogin("admin", password="123456")
-    print(cli.recv_data)
+    cli.userLogin("admin", password="1234s56")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # @FileName: Main.py
 # coding: utf-8
 
-import sys
+import sys, socket
 
 from PyQt6.QtCore import QLocale, QThread, pyqtSignal
 from PyQt6.QtWidgets import QApplication
@@ -16,11 +16,12 @@ from client import Client
 
 linkState = None
 
-class LinkThread(QThread):
-    data_signal = pyqtSignal(dict)
+class LinkServerThread(QThread):
+    """连接服务器子线程"""
+    state_socketObj_signal = pyqtSignal(list)
 
     def __init__(self, address):
-        super(LinkThread, self).__init__()
+        super(LinkServerThread, self).__init__()
         self.address = address
         tranAddress = (str(self.address[0]),int(self.address[1]))
         self.clientSocket = Client(*tranAddress)
@@ -28,11 +29,10 @@ class LinkThread(QThread):
     def run(self):
         try:
             self.clientSocket.connectServer()
-            self.data_signal.emit(self.clientSocket.clientstate)
-        except (TypeError, ValueError, OSError, UnboundLocalError) as e:
-            self.data_signal.emit({'errorcode': e})
- 
-        
+            self.state_socketObj_signal.emit([self.clientSocket.clientstate, self.clientSocket])
+        except (TypeError, ValueError, OSError, UnboundLocalError, ConnectionRefusedError, ConnectionError) as e:
+            self.state_socketObj_signal.emit([False,e])
+
 class MainClient():
     def __init__(self):
         self.QtApp = QApplication(sys.argv)
@@ -40,37 +40,53 @@ class MainClient():
         self.QtApp.installTranslator(translator)
     
         self.login_w = LoginUI()
+        self.main_w = MainUI()
+
+    def __linkisSuccess(self, state_socket):
+        if state_socket[0]:
+            InfoBarDisplay(self.login_w, type="info", whereis="TOP", title="连接成功!", infomation="")
+            self.client_socket_obj = state_socket[1]
+            self.login_w.setLoginState(1)
+        elif not state_socket[0]:
+            InfoBarDisplay(self.login_w, type="error", whereis="TOP_RIGHT", title="错误", infomation=f"{state_socket[1]}")
 
     def linkServerFunction(self,address):
-        def isSuccess(state):
-            if state["errorcode"] == 300:
-                InfoBarDisplay(self.login_w, type="info", whereis="TOP", title="连接成功!", infomation="")
-                self.login_w.setLoginState(1)
-                
-            else:
-                InfoBarDisplay(self.login_w, type="error", whereis="TOP", title="错误", infomation=f"{state}")
         if not address[0] or not address[1]:
-            InfoBarDisplay(self.login_w, type="warn", whereis="TOP", title="警告", infomation="地址不得为空")
+            InfoBarDisplay(self.login_w, type="warn", whereis="TOP_RIGHT", title="警告", infomation="地址不得为空")
         elif not isinstance(address[1], str):
             print(isinstance(address[1], int))
-            InfoBarDisplay(self.login_w, type="warn", whereis="TOP", title="警告", infomation="地址格式有误")
+            InfoBarDisplay(self.login_w, type="warn", whereis="TOP_RIGHT", title="警告", infomation="地址格式有误")
         else:
-            print(1)
-            self.linkThread = LinkThread(address)
-            self.linkThread.start()
-            self.linkThread.data_signal.connect(isSuccess)
-            
+            print("Connecting......")
+            self.linkServerThread = LinkServerThread(address)
+            self.linkServerThread.start()
+            self.linkServerThread.state_socketObj_signal.connect(self.__linkisSuccess)
+
+    def userLoginFunction(self, account):
+        self.client_socket_obj.userLogin(*account)
+        print(self.client_socket_obj.state_token)
+        if self.client_socket_obj.state_token == "time out":
+            InfoBarDisplay(self.login_w, type="warn", whereis="TOP", title="登陆超时", infomation="")
+        elif self.client_socket_obj.state_token["code"] == 0:
+            self.token = self.client_socket_obj.state_token["token"]
+            self.login_w.close()
+            self.main_w.mainUI()
+            InfoBarDisplay(self.main_w, type="info", whereis="TOP", title="登陆成功!", infomation="")
+        elif self.client_socket_obj.state_token["code"] == 401:
+            msg = self.client_socket_obj.state_token["msg"]
+            InfoBarDisplay(self.login_w, type="warn", whereis="TOP", title="登陆失败。", infomation=f"{msg}")
+
     def backAct(self):
         self.login_w.setLoginState(0)
-        self.clientSocket.clientClose()
+        self.client_socket.close()
         print("Socket has been closed.")
 
     def main(self):
-
         self.login_w.setLoginState(0)
         self.login_w.loginUI()
         self.login_w.link_server_button.clicked.connect(lambda: self.linkServerFunction(self.login_w.getServerAddess()))
         self.login_w.back_Button.clicked.connect(lambda: self.backAct())
+        self.login_w.login_Button.clicked.connect(lambda: self.userLoginFunction(self.login_w.getUserAccount()))
         # 启动Qt应用程序
         self.QtApp.exec()
     
